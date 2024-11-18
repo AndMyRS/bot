37382c44b4634d612c0ada33ea38cac57cc3b75e
Vladimir Andrusevich <vl.andrusevich@yandex.by>
2024-11-18 22:19:51 +0300

    Add recover

--- internal/app/commander/commander.go
+++ internal/app/commander/commander.go
@@ -1,6 +1,8 @@
 package commander
 
 import (
+	"log"
+
 	"github.com/AndMyRs/bot/internal/service/product"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
@@ -18,6 +20,13 @@ func NewCommander(bot *tgbotapi.BotAPI, productService *product.Service) *Comman
 }
 
 func (c *Commander) HandleUpdate(update tgbotapi.Update) {
+	defer func() {
+		if panicValue := recover(); panicValue != nil {
+			log.Printf("recovered from panic: %v", panicValue)
+		}
+
+	}()
+
 	if update.Message != nil { // If we got a message
 
 		//command, ok := registeredCommands[update.Message.Command()]
--- internal/app/commander/get.go
+++ internal/app/commander/get.go
@@ -1,11 +1,30 @@
 package commander
 
 import (
+	"log"
+	"strconv"
+
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func (c *Commander) Get(inputMsg *tgbotapi.Message) {
 
-	msg := tgbotapi.NewMessage(inputMsg.Chat.ID, "TBD")
+	args := inputMsg.CommandArguments()
+
+	idx, err := strconv.Atoi(args)
+
+	if err != nil {
+		log.Println("wrong args", args)
+		return
+	}
+
+	product, err := c.productService.Get(idx)
+
+	if err != nil {
+		log.Printf("failed to get product with id [%d]: %v", idx, err)
+		return
+	}
+
+	msg := tgbotapi.NewMessage(inputMsg.Chat.ID, product.Title)
 	c.bot.Send(msg)
 }
